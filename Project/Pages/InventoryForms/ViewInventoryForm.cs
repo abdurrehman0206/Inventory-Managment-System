2681928cba36cddf3426c95644ffff20ed782c01
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2024-01-07 15:00:39 +0500

    Added View Inventory Functionality

--- Project/Pages/InventoryForms/ViewInventoryForm.cs
+++ Project/Pages/InventoryForms/ViewInventoryForm.cs
@@ -1,10 +1,32 @@
-﻿namespace Project.Pages.InventoryForms
+﻿using Project.Controllers;
+using Project.Models;
+
+namespace Project.Pages.InventoryForms
 {
     public partial class ViewInventoryForm : Form
     {
+        private readonly ProductController _productController;
         public ViewInventoryForm()
         {
             InitializeComponent();
+            _productController = new ProductController();
+        }
+        private void LoadProducts()
+        {
+            try
+            {
+                List<Product> products = _productController.GetAllProducts();
+                dgvProduct.DataSource = products;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading products: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ViewInventoryForm_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
         }
     }
 }
