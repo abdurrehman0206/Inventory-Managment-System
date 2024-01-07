d1918fcc91f70495456655736156255b07e25ca0
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2024-01-07 22:22:31 +0500

    Added Place Order Functionality

--- Project/Pages/OrdersForms/PlaceOrderForm.Designer.cs
+++ Project/Pages/OrdersForms/PlaceOrderForm.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             lblPlaceOrder = new Label();
@@ -39,15 +38,20 @@
             lblAddress = new Label();
             pnlAddress = new Panel();
             txtAddress = new TextBox();
-            dgvOrder = new DataGridView();
-            ProductID = new DataGridViewTextBoxColumn();
-            Quantity = new DataGridViewTextBoxColumn();
             lblSelect = new Label();
             lblAmount = new Label();
             btnPlaceOrder = new Button();
+            cmbProducts = new ComboBox();
+            btnAddProduct = new Button();
+            lblQuantity = new Label();
+            pnlQuantity = new Panel();
+            txtQuantity = new TextBox();
+            dgvOrderDetails = new DataGridView();
+            lblSelectProd = new Label();
             pnlName.SuspendLayout();
             pnlAddress.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvOrder).BeginInit();
+            pnlQuantity.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvOrderDetails).BeginInit();
             SuspendLayout();
             // 
             // lblPlaceOrder
@@ -70,7 +74,8 @@
             lblName.BackColor = Color.Transparent;
             lblName.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblName.ForeColor = Color.Black;
-            lblName.Location = new Point(58, 101);
+            lblName.Location = new Point(46, 90);
+            lblName.Margin = new Padding(5);
             lblName.Name = "lblName";
             lblName.Size = new Size(133, 21);
             lblName.TabIndex = 3;
@@ -81,7 +86,7 @@
             pnlName.Anchor = AnchorStyles.None;
             pnlName.BackColor = Color.FromArgb(225, 217, 242);
             pnlName.Controls.Add(txtName);
-            pnlName.Location = new Point(58, 135);
+            pnlName.Location = new Point(46, 119);
             pnlName.Name = "pnlName";
             pnlName.Size = new Size(227, 40);
             pnlName.TabIndex = 8;
@@ -106,7 +111,8 @@
             lblAddress.BackColor = Color.Transparent;
             lblAddress.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblAddress.ForeColor = Color.Black;
-            lblAddress.Location = new Point(497, 101);
+            lblAddress.Location = new Point(279, 90);
+            lblAddress.Margin = new Padding(5);
             lblAddress.Name = "lblAddress";
             lblAddress.Size = new Size(143, 21);
             lblAddress.TabIndex = 9;
@@ -117,7 +123,7 @@
             pnlAddress.Anchor = AnchorStyles.None;
             pnlAddress.BackColor = Color.FromArgb(225, 217, 242);
             pnlAddress.Controls.Add(txtAddress);
-            pnlAddress.Location = new Point(497, 135);
+            pnlAddress.Location = new Point(279, 119);
             pnlAddress.Name = "pnlAddress";
             pnlAddress.Size = new Size(227, 40);
             pnlAddress.TabIndex = 10;
@@ -135,75 +141,6 @@
             txtAddress.MouseClick += txtAddress_MouseClick;
             txtAddress.Leave += txtAddress_Leave;
             // 
-            // dgvOrder
-            // 
-            dgvOrder.AllowUserToResizeColumns = false;
-            dgvOrder.AllowUserToResizeRows = false;
-            dgvOrder.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            dgvOrder.BackgroundColor = Color.FromArgb(225, 217, 242);
-            dgvOrder.BorderStyle = BorderStyle.None;
-            dgvOrder.CellBorderStyle = DataGridViewCellBorderStyle.Raised;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = Color.FromArgb(225, 217, 242);
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle1.Padding = new Padding(5);
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dgvOrder.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            dgvOrder.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvOrder.Columns.AddRange(new DataGridViewColumn[] { ProductID, Quantity });
-            dgvOrder.EnableHeadersVisualStyles = false;
-            dgvOrder.GridColor = Color.FromArgb(225, 217, 242);
-            dgvOrder.Location = new Point(58, 328);
-            dgvOrder.Margin = new Padding(0);
-            dgvOrder.Name = "dgvOrder";
-            dgvOrder.RowHeadersVisible = false;
-            dataGridViewCellStyle4.Padding = new Padding(5);
-            dgvOrder.RowsDefaultCellStyle = dataGridViewCellStyle4;
-            dgvOrder.RowTemplate.DefaultCellStyle.Padding = new Padding(5);
-            dgvOrder.RowTemplate.Height = 30;
-            dgvOrder.ShowEditingIcon = false;
-            dgvOrder.Size = new Size(666, 143);
-            dgvOrder.TabIndex = 11;
-            dgvOrder.Leave += dgvOrder_Leave;
-            dgvOrder.MouseClick += dgvOrder_MouseClick;
-            // 
-            // ProductID
-            // 
-            ProductID.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(225, 217, 242);
-            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle2.ForeColor = Color.Black;
-            dataGridViewCellStyle2.Padding = new Padding(5);
-            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(163, 103, 177);
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.Control;
-            ProductID.DefaultCellStyle = dataGridViewCellStyle2;
-            ProductID.HeaderText = "ProductID";
-            ProductID.Name = "ProductID";
-            ProductID.Resizable = DataGridViewTriState.False;
-            ProductID.ToolTipText = "Enter Product ID";
-            // 
-            // Quantity
-            // 
-            Quantity.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = Color.FromArgb(225, 217, 242);
-            dataGridViewCellStyle3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle3.ForeColor = Color.Black;
-            dataGridViewCellStyle3.Format = "N3";
-            dataGridViewCellStyle3.NullValue = null;
-            dataGridViewCellStyle3.Padding = new Padding(5);
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(163, 103, 177);
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.Control;
-            Quantity.DefaultCellStyle = dataGridViewCellStyle3;
-            Quantity.HeaderText = "Quantity";
-            Quantity.Name = "Quantity";
-            Quantity.Resizable = DataGridViewTriState.False;
-            Quantity.ToolTipText = "Enter Product Quantity";
-            // 
             // lblSelect
             // 
             lblSelect.Anchor = AnchorStyles.None;
@@ -211,11 +148,12 @@
             lblSelect.BackColor = Color.Transparent;
             lblSelect.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblSelect.ForeColor = Color.Black;
-            lblSelect.Location = new Point(58, 257);
+            lblSelect.Location = new Point(46, 275);
+            lblSelect.Margin = new Padding(5);
             lblSelect.Name = "lblSelect";
-            lblSelect.Size = new Size(127, 21);
+            lblSelect.Size = new Size(146, 21);
             lblSelect.TabIndex = 12;
-            lblSelect.Text = "Select Products";
+            lblSelect.Text = "Selected Products";
             // 
             // lblAmount
             // 
@@ -224,7 +162,7 @@
             lblAmount.BackColor = Color.Transparent;
             lblAmount.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             lblAmount.ForeColor = Color.Black;
-            lblAmount.Location = new Point(58, 523);
+            lblAmount.Location = new Point(46, 589);
             lblAmount.Margin = new Padding(0);
             lblAmount.Name = "lblAmount";
             lblAmount.Size = new Size(196, 25);
@@ -241,24 +179,162 @@
             btnPlaceOrder.FlatStyle = FlatStyle.Flat;
             btnPlaceOrder.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnPlaceOrder.ForeColor = SystemColors.Control;
-            btnPlaceOrder.Location = new Point(323, 622);
+            btnPlaceOrder.Location = new Point(323, 704);
             btnPlaceOrder.Name = "btnPlaceOrder";
             btnPlaceOrder.Size = new Size(149, 45);
             btnPlaceOrder.TabIndex = 14;
             btnPlaceOrder.Tag = "";
             btnPlaceOrder.Text = "Place Order";
             btnPlaceOrder.UseVisualStyleBackColor = false;
+            btnPlaceOrder.Click += btnPlaceOrder_Click;
+            // 
+            // cmbProducts
+            // 
+            cmbProducts.Anchor = AnchorStyles.None;
+            cmbProducts.BackColor = Color.FromArgb(225, 217, 242);
+            cmbProducts.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbProducts.FlatStyle = FlatStyle.Flat;
+            cmbProducts.FormattingEnabled = true;
+            cmbProducts.ItemHeight = 15;
+            cmbProducts.Location = new Point(46, 217);
+            cmbProducts.Name = "cmbProducts";
+            cmbProducts.Size = new Size(693, 23);
+            cmbProducts.Sorted = true;
+            cmbProducts.TabIndex = 15;
+            // 
+            // btnAddProduct
+            // 
+            btnAddProduct.Anchor = AnchorStyles.None;
+            btnAddProduct.BackColor = Color.FromArgb(57, 36, 103);
+            btnAddProduct.FlatAppearance.BorderColor = Color.FromArgb(57, 36, 135);
+            btnAddProduct.FlatAppearance.BorderSize = 2;
+            btnAddProduct.FlatAppearance.MouseOverBackColor = Color.FromArgb(163, 103, 177);
+            btnAddProduct.FlatStyle = FlatStyle.Flat;
+            btnAddProduct.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAddProduct.ForeColor = SystemColors.Control;
+            btnAddProduct.Location = new Point(323, 540);
+            btnAddProduct.Name = "btnAddProduct";
+            btnAddProduct.Size = new Size(149, 45);
+            btnAddProduct.TabIndex = 16;
+            btnAddProduct.Tag = "";
+            btnAddProduct.Text = "Add Product";
+            btnAddProduct.UseVisualStyleBackColor = false;
+            btnAddProduct.Click += btnAddProduct_Click;
+            // 
+            // lblQuantity
+            // 
+            lblQuantity.Anchor = AnchorStyles.None;
+            lblQuantity.AutoSize = true;
+            lblQuantity.BackColor = Color.Transparent;
+            lblQuantity.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblQuantity.ForeColor = Color.Black;
+            lblQuantity.Location = new Point(512, 90);
+            lblQuantity.Margin = new Padding(5);
+            lblQuantity.Name = "lblQuantity";
+            lblQuantity.Size = new Size(77, 21);
+            lblQuantity.TabIndex = 11;
+            lblQuantity.Text = "Quantity";
+            // 
+            // pnlQuantity
+            // 
+            pnlQuantity.Anchor = AnchorStyles.None;
+            pnlQuantity.BackColor = Color.FromArgb(225, 217, 242);
+            pnlQuantity.Controls.Add(txtQuantity);
+            pnlQuantity.Location = new Point(512, 119);
+            pnlQuantity.Name = "pnlQuantity";
+            pnlQuantity.Size = new Size(227, 40);
+            pnlQuantity.TabIndex = 12;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.BackColor = Color.FromArgb(225, 217, 242);
+            txtQuantity.BorderStyle = BorderStyle.None;
+            txtQuantity.Font = new Font("Segoe UI Light", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txtQuantity.Location = new Point(10, 12);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.PlaceholderText = "Quantity";
+            txtQuantity.Size = new Size(210, 16);
+            txtQuantity.TabIndex = 5;
+            // 
+            // dgvOrderDetails
+            // 
+            dgvOrderDetails.AllowUserToDeleteRows = false;
+            dgvOrderDetails.AllowUserToOrderColumns = true;
+            dgvOrderDetails.AllowUserToResizeColumns = false;
+            dgvOrderDetails.AllowUserToResizeRows = false;
+            dgvOrderDetails.Anchor = AnchorStyles.None;
+            dgvOrderDetails.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvOrderDetails.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvOrderDetails.BackgroundColor = Color.Lavender;
+            dgvOrderDetails.BorderStyle = BorderStyle.None;
+            dgvOrderDetails.CellBorderStyle = DataGridViewCellBorderStyle.Raised;
+            dgvOrderDetails.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(135, 104, 202);
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.Padding = new Padding(5);
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvOrderDetails.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvOrderDetails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvOrderDetails.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvOrderDetails.EnableHeadersVisualStyles = false;
+            dgvOrderDetails.GridColor = Color.FromArgb(225, 217, 242);
+            dgvOrderDetails.Location = new Point(46, 301);
+            dgvOrderDetails.Margin = new Padding(0);
+            dgvOrderDetails.Name = "dgvOrderDetails";
+            dgvOrderDetails.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = Color.Lavender;
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.Padding = new Padding(5);
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(225, 217, 242);
+            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
+            dgvOrderDetails.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dgvOrderDetails.RowTemplate.DefaultCellStyle.Padding = new Padding(5);
+            dgvOrderDetails.RowTemplate.Height = 30;
+            dgvOrderDetails.RowTemplate.ReadOnly = true;
+            dgvOrderDetails.ShowEditingIcon = false;
+            dgvOrderDetails.Size = new Size(693, 236);
+            dgvOrderDetails.TabIndex = 17;
+            // 
+            // lblSelectProd
+            // 
+            lblSelectProd.Anchor = AnchorStyles.None;
+            lblSelectProd.AutoSize = true;
+            lblSelectProd.BackColor = Color.Transparent;
+            lblSelectProd.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSelectProd.ForeColor = Color.Black;
+            lblSelectProd.Location = new Point(46, 193);
+            lblSelectProd.Name = "lblSelectProd";
+            lblSelectProd.Size = new Size(120, 21);
+            lblSelectProd.TabIndex = 18;
+            lblSelectProd.Text = "Select Product";
             // 
             // PlaceOrderForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
             BackColor = Color.Lavender;
             ClientSize = new Size(784, 761);
+            Controls.Add(lblSelectProd);
+            Controls.Add(dgvOrderDetails);
+            Controls.Add(pnlQuantity);
+            Controls.Add(lblQuantity);
+            Controls.Add(btnAddProduct);
+            Controls.Add(cmbProducts);
             Controls.Add(btnPlaceOrder);
             Controls.Add(lblAmount);
             Controls.Add(lblSelect);
-            Controls.Add(dgvOrder);
             Controls.Add(pnlAddress);
             Controls.Add(lblAddress);
             Controls.Add(pnlName);
@@ -270,7 +346,9 @@
             pnlName.PerformLayout();
             pnlAddress.ResumeLayout(false);
             pnlAddress.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvOrder).EndInit();
+            pnlQuantity.ResumeLayout(false);
+            pnlQuantity.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvOrderDetails).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -285,11 +363,19 @@
         private Label lblAddress;
         private Panel pnlAddress;
         private TextBox txtAddress;
-        private DataGridView dgvOrder;
+        private DataGridView dgvOrderDetails;
+        private Label lblSelectProd;
         private Label lblSelect;
         private Label lblAmount;
         private DataGridViewTextBoxColumn ProductID;
         private DataGridViewTextBoxColumn Quantity;
         private Button btnPlaceOrder;
+        private ComboBox cmbProducts;
+        private Button btnAddProduct;
+        private Panel panel1;
+        private Label lblQuantity;
+        private Panel pnlQuantity;
+        private TextBox txtQuantity;
+        private DataGridView dgvOrder;
     }
 }
--- Project/Pages/OrdersForms/PlaceOrderForm.cs
+++ Project/Pages/OrdersForms/PlaceOrderForm.cs
@@ -1,10 +1,30 @@
-﻿namespace Project.Pages.OrdersForms
+﻿using MongoDB.Bson;
+using Project.Controllers;
+using Project.Models;
+
+namespace Project.Pages.OrdersForms
 {
     public partial class PlaceOrderForm : Form
     {
+        private readonly OrderController _orderController;
+        private List<Product> _availableProducts;
         public PlaceOrderForm()
         {
             InitializeComponent();
+            _orderController = new OrderController();
+            _availableProducts = _orderController.GetAvailableProducts();
+
+            cmbProducts.DataSource = _availableProducts;
+            cmbProducts.DisplayMember = "Name";
+            cmbProducts.ValueMember = "_id";
+            dgvOrderDetails.Columns.Add("_id", "ProductId");
+            dgvOrderDetails.Columns.Add("Name", "Product Name");
+            dgvOrderDetails.Columns.Add("Description", "Description");
+            dgvOrderDetails.Columns.Add("Quantity", "Quantity");
+            dgvOrderDetails.Columns.Add("Category", "Category");
+            dgvOrderDetails.Columns.Add("Price", "Price");
+            dgvOrderDetails.Columns["Name"].ReadOnly = true;
+            dgvOrderDetails.Columns["Price"].ReadOnly = true;
         }
 
         //txtName Events
@@ -35,21 +55,113 @@
         //dgvOrder Events
         private void dgvOrder_MouseClick(object sender, MouseEventArgs e)
         {
-            dgvOrder.BorderStyle = BorderStyle.FixedSingle;
+            dgvOrderDetails.BorderStyle = BorderStyle.FixedSingle;
         }
 
         private void dgvOrder_Leave(object sender, EventArgs e)
         {
-            dgvOrder.BorderStyle = BorderStyle.None;
-            if (dgvOrder.SelectedCells.Count > 0)
+            dgvOrderDetails.BorderStyle = BorderStyle.None;
+            if (dgvOrderDetails.SelectedCells.Count > 0)
             {
 
-                dgvOrder.ClearSelection();
+                dgvOrderDetails.ClearSelection();
             }
         }
 
+        private void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+            if (dgvOrderDetails.Rows.Count == 1)
+            {
+                MessageBox.Show("Please add products to the order before placing it.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+
+                Order order = new Order
+                {
+                    CustomerName = txtName.Text,
+                    ShippingAddress = txtAddress.Text,
+                    IsShipped = false,
+                    ShippedDate = DateTime.MinValue
+                };
 
 
+                for (int i = 0; i < dgvOrderDetails.Rows.Count - 1; i++)
+                {
+                    DataGridViewRow row = dgvOrderDetails.Rows[i];
 
+                    if (row.Cells["_id"].Value != null &&
+                        row.Cells["Name"].Value != null &&
+                        row.Cells["Price"].Value != null &&
+                        row.Cells["Quantity"].Value != null &&
+                        row.Cells["Category"].Value != null &&
+                        row.Cells["Description"].Value != null)
+                    {
+                        Product orderedProduct = new Product
+                        {
+                            _id = (BsonObjectId)row.Cells["_id"].Value,
+                            Name = (string)row.Cells["Name"].Value,
+                            Price = (double)row.Cells["Price"].Value,
+                            Quantity = (int)row.Cells["Quantity"].Value,
+                            Category = (string)row.Cells["Category"].Value,
+                            Description = (string)row.Cells["Description"].Value,
+                        };
+
+                        _orderController.AddProductToOrder(order, orderedProduct, orderedProduct.Quantity);
+                    }
+                    else
+                    {
+                        MessageBox.Show("One or more cell values are null. Cannot create the ordered product.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                _orderController.PlaceOrder(order);
+                txtName.Clear();
+                txtQuantity.Clear();
+                txtAddress.Clear();
+                dgvOrderDetails.Rows.Clear();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Error placing order: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            if (cmbProducts.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a product.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(txtQuantity.Text, out int quantity) || quantity <= 0)
+            {
+                MessageBox.Show("Please enter a valid quantity.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var selectedProduct = (Product)cmbProducts.SelectedItem;
+            bool productExists = false;
+            foreach (DataGridViewRow row in dgvOrderDetails.Rows)
+            {
+                if (row.Cells["_id"].Value != null && row.Cells["_id"].Value.Equals(selectedProduct._id))
+                {
+                    int existingQuantity = (int)row.Cells["Quantity"].Value;
+                    row.Cells["Quantity"].Value = existingQuantity + quantity;
+
+                    productExists = true;
+                    break;
+                }
+            }
+
+            if (!productExists)
+            {
+                dgvOrderDetails.Rows.Add(selectedProduct._id, selectedProduct.Name, selectedProduct.Description, quantity, selectedProduct.Category, selectedProduct.Price);
+            }
+            cmbProducts.SelectedIndex = -1;
+            txtQuantity.Clear();
+        }
     }
 }
