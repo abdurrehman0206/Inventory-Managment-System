a3f2b4a42ca2a9b6f807e2b010b68154c81deb66
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2024-01-10 00:20:07 +0500

    "merged"

--- Project/Pages/DashboardForms/DashboardForm.Designer.cs
+++ Project/Pages/DashboardForms/DashboardForm.Designer.cs
@@ -28,18 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(225, 217, 242);
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(21, 45);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(187, 71);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(86, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 23);
+            label2.TabIndex = 1;
+            label2.Text = "$ 0.0";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkGray;
+            label1.Location = new Point(86, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Total Sales";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // DashboardForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 800);
+            Controls.Add(panel1);
             Name = "DashboardForm";
             Text = "DashboardForm";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- Project/Pages/DashboardForms/DashboardForm.cs
+++ Project/Pages/DashboardForms/DashboardForm.cs
@@ -16,5 +16,7 @@ namespace Project
         {
             InitializeComponent();
         }
+
+      
     }
 }
